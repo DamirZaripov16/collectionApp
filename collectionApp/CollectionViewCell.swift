8aa866c177a6ec741baf5925f54ca4b32a436846
Damir Zaripov <zaripov.damir92@mail.ru>
2022-10-29 18:33:26 +0300

    initial commit

---
+++ collectionApp/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  collectionApp
+//
+//  Created by Damirka on 29.10.2022.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var imageLabel: UILabel!
+    
+}
---
+++ collectionApp/ImageViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ImageViewModel.swift
+//  Collection
+//
+//  Created by Damirka on 29.10.2022.
+//
+
+import Foundation
+
+struct ImageModel {
+    
+    var image: String = ""
+    var label: String = ""
+    
+    init(image: String, label: String){
+        
+        self.image = image
+        self.label = label
+        
+    }
+}
